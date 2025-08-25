59fc8309843f4d254d0bb65b98eb93daddf02de2
Bharath <bharthkamatham143@gmail.com>
2025-08-25 07:11:24 +0530

    Added data for navigation links and also social media links.

---
+++ src/project data/NavLinks.js
@@ -0,0 +1,14 @@
+const NavLinks = [
+  { title: "HOME" },
+  { title: "ABOUT ME" },
+  { title: "PROJECTS" },
+  { title: "CONNECT" },
+];
+
+const SocialMedia = [
+  { title: "RESUME", link: "/Resume.pdf" },
+  { title: "GITHUB", link: "https://github.com/Bharath-2101" },
+  { title: "LINKEDIN", link: "https://www.linkedin.com/in/bharthkumark/" },
+  { title: "DRIBBLE", link: "https://dribbble.com/iamgoodman9" },
+];
+export { NavLinks, SocialMedia };
