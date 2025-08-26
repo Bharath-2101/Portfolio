a3ae41c6df6db506131110df6f490f6266d55f69
Bharath <bharthkamatham143@gmail.com>
2025-08-26 07:27:55 +0530

    Added Hero Section into index Page.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,16 @@
+import Hamburger from "@/components/Hamburger/Hamburger";
+import HeroSection from "@/sections/HeroSection/HeroSection";
+import NavMenu from "@/sections/NavMenu/NavMenu";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <NavMenu />
+      <HeroSection />
+      <div className="h-[100vh]" />
+    </div>
+  );
 };
 
 export default page;
