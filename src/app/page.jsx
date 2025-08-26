59e12de9d417cf7d63b04b90cb8c0f76c12e1729
Bharath <bharthkamatham143@gmail.com>
2025-08-26 11:04:30 +0530

    added Projects section into the project.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,46 @@
-import Hamburger from "@/components/Hamburger/Hamburger";
+"use client";
 import HeroSection from "@/sections/HeroSection/HeroSection";
 import NavMenu from "@/sections/NavMenu/NavMenu";
-import React from "react";
+import Projects from "@/sections/projects/Projects";
+import React, { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
 
 const page = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      touchMultiplier: 2,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove((time) => lenis.raf(time * 1000));
+    };
+  }, []);
+
   return (
     <div>
       <NavMenu />
       <HeroSection />
-      <div className="h-[100vh]" />
+      <Projects />
+      <div className="h-[1000px]" />
     </div>
   );
 };
