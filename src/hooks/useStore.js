062eae197378a594e1f36228bc5df653282b9519
Bharath <bharthkamatham143@gmail.com>
2025-08-26 07:30:31 +0530

    Added Static height for mobile and Tablet devices.

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,9 +1,15 @@
+"use client";
 import { create } from "zustand";
 
 const useStore = create((set) => ({
   hamburgerClick: false,
   changeHamburgerClick: () =>
     set((state) => ({ hamburgerClick: !state.hamburgerClick })),
+
+  height: 0,
+  width: 0,
+
+  setDimensions: (height, width) => set({ height, width }),
 }));
 
 export default useStore;
