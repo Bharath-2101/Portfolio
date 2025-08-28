b8879bbdcc1186f0cd7c79c14d8d0c49c5918ae6
Bharath <bharthkamatham143@gmail.com>
2025-08-28 19:43:54 +0530

    Added Archives Data.

---
+++ src/project data/Archives.js
@@ -0,0 +1,34 @@
+const archives = [
+  {
+    title: "Disjointed Image Effect",
+    category: "Awwwards Inspiration",
+    year: 2025,
+    link: "https://disjointed-image-effect.netlify.app/",
+  },
+  {
+    title: "Sticky Footer",
+    category: "Awwwards Inspiration",
+    year: 2025,
+    link: "https://stick-footer.netlify.app/",
+  },
+  {
+    title: "Three Js Models",
+    category: "Practice Work",
+    year: 2025,
+    link: "https://github.com/Bharath-2101/Three-JS-Models-ShowCase",
+  },
+  {
+    title: "Curved Side Menu",
+    category: "Awwwards Inspiration",
+    year: 2025,
+    link: "https://curvedsidemenu.netlify.app/",
+  },
+  {
+    title: "Pomodoro",
+    category: "Mobile App",
+    year: 2024,
+    link: "https://github.com/Bharath-2101/Pomodoro-native",
+  },
+];
+
+export { archives };
