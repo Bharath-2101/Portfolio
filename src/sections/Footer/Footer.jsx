f0c15be6cac959526373e391042ff74410548699
Bharath <bharthkamatham143@gmail.com>
2025-08-29 14:09:39 +0530

    Added Footer component for project.

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,80 @@
+"use client";
+import React from "react";
+import styles from "./styles.module.scss";
+import { NavLinks, SocialMedia } from "@/project data";
+
+const Footer = () => {
+  return (
+    <div className={styles.MainContainer}>
+      <div className={styles.SVGCon}>
+        <svg viewBox="0 0 100 100" preserveAspectRatio="none">
+          <path
+            d="M0 100 L0 50 L30 50 L35 0 L65 0 L70 50 L100 50 L100 100 Z"
+            fill="#efefef"
+          />
+          <ellipse
+            cx="42"
+            cy="40"
+            rx="1"
+            ry="10"
+            fill="#121212"
+            stroke="#121212"
+          />
+          <ellipse
+            cx="58"
+            cy="40"
+            rx="1"
+            ry="10"
+            fill="#121212"
+            stroke="#121212"
+          />
+        </svg>
+      </div>
+
+      {/* Location */}
+      <div className={styles.LocationContainer}>
+        <div>
+          <span>LOCATION</span>
+        </div>
+        <div>
+          <span>Currently Based in INDIA</span>
+        </div>
+      </div>
+
+      {/* Navigation */}
+      <div className={styles.NavigationContainer}>
+        <div>
+          <span>NAVIGATION</span>
+        </div>
+        <div className={styles.LinksCon}>
+          {NavLinks.map((link, item) => (
+            <div className={"UnderlineEffect"} key={item}>
+              {link.title}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Socials */}
+      <div className={styles.SocialsContainer}>
+        <div>
+          <span>SOCIALS</span>
+        </div>
+        <div className={styles.SocialLinksCon}>
+          {SocialMedia.map((link, item) => (
+            <a href={link.link} className={"UnderlineEffect"} key={item}>
+              {link.title}
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.CopyRight}>
+        <span>2025 &copy;</span>
+        <span>Made By Bharath Kumar</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
