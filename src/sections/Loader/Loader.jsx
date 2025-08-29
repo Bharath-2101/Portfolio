a99fdc8812fccc2dd0f9616f0b6a24162b348c1b
Bharath <bharthkamatham143@gmail.com>
2025-08-29 22:04:59 +0530

    Changed the animation and also added static height for Container instead of dynamic height.

--- src/sections/Loader/Loader.jsx
+++ src/sections/Loader/Loader.jsx
@@ -2,51 +2,61 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "./styles.module.scss";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import MorphSVGPlugin from "gsap/MorphSVGPlugin";
 
 const Loader = ({ loader, setLoader }) => {
   const MainRef = useRef();
   const stepsRef = useRef([]);
-  const [height, setHeight] = useState(0);
+  const pathRefs = useRef([]);
 
   useGSAP(() => {
-    if (stepsRef.current.length > 0) {
+    gsap.registerPlugin(MorphSVGPlugin);
+    if (stepsRef.current.length > 0 && pathRefs.current.length > 0) {
       gsap
         .timeline({
           onComplete: () => {
             setLoader(false);
           },
         })
-        .to(stepsRef.current, {
-          yPercent: -100,
-          stagger: 0.17,
-          ease: "cubic-bezier(0.76, 0, 0.24, 1)",
-        })
+        .to(
+          stepsRef.current,
+          {
+            yPercent: -100,
+            stagger: 0.17,
+            ease: "cubic-bezier(0.76, 0, 0.24, 1)",
+          },
+          0
+        )
+        .to(
+          pathRefs.current,
+          {
+            morphSVG: "M0 0 L100 0 L100 100 Q50 100 0 100 Z",
+            stagger: 0.17,
+            ease: "cubic-bezier(0.76, 0, 0.24, 1)",
+          },
+          0
+        )
         .to(MainRef.current, { yPercent: -100, duration: 0.01 });
     }
   }, []);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setHeight(
-        window.innerHeight + (window.screen.height - window.innerHeight) * 0.62
-      );
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <div ref={MainRef} style={{ height }} className={styles.MainContainer}>
+    <div ref={MainRef} className={styles.MainContainer}>
       {[...Array(5)].map((_, i) => (
-        <div
+        <svg
           key={i}
           ref={(el) => (stepsRef.current[i] = el)}
           className={styles.Step}
-        />
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+          stroke="none"
+        >
+          <path
+            ref={(el) => (pathRefs.current[i] = el)}
+            d="M0 0 L100 0 L100 100 Q50 50 0 100 Z"
+            fill="#efefef"
+          />
+        </svg>
       ))}
     </div>
   );
